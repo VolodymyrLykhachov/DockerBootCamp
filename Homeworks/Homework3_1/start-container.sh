12c5b26bbb1cd8b686273c1b6ee47900c9d22de6
vlykh <vlykh@softserveinc.com>
2018-10-17 20:56:47 +0300

    add second part of dockerfile

--- Homeworks/Homework3_1/start-container.sh
+++ Homeworks/Homework3_1/start-container.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#create network
+docker network create --driver bridge testing_network
 #start application container
-docker run --name homework3_1 -t  -i  -v e:/logs:/flask/logs -p 5000:5000 --rm  violant/hits:h3
+docker run --name hits -t  -i  -v e:/logs:/flask/logs -p 5000:5000 --network testing_network --rm  violant/hits:h3
 #start app test container
+docker run --network testing_network violant/hits-at:h3
---
+++ Homeworks/Homework3_2/start-container.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#start Jenkins
+docker run -p 8080:8080 -p 50000:50000 jenkins
