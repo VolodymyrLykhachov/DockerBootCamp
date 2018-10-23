9cf7255bac6a237485a1f6388772806668b5c773
vlykh <vlykh@softserveinc.com>
2018-10-23 13:45:09 +0300

    add homework 4

---
+++ Homeworks/Homework4_1/start-compose.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#start compose
+docker-compose up --build --scale hits=3
---
+++ Homeworks/Homework4_1/tests/test_app.py
@@ -0,0 +1,22 @@
+import pytest
+import requests
+import time
+
+
+@pytest.fixture
+def url(request):
+  url = request.config.getoption("--url")
+  if not url:
+    raise ValueError('HITS URL was not provided. Use \'--url\' option to set up')
+  return url
+
+def test_default_url(url):
+  time.sleep(10)
+  response = requests.get(url)
+  assert len(response.text) > 0, 'The response is empty'
+
+
+def test_logs(url):
+  time.sleep(10)
+  response = requests.get(url + '/logs')
+  assert 'My Hostname is:' in response.text, 'Logs do not have Hostname'
